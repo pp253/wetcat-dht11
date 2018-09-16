676218facf47fa6d181da80b103528d341cd250b
pp253 <pp.pp253@gmail.com>
2018-09-16 12:01:05 +0800

    modified main setting, add the package.json

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import ASUS.GPIO as GPIO
-import lib/dht11
+import dht11
 import time
 import datetime
 import requests
@@ -12,21 +12,22 @@ GPIO.cleanup()
 # read data using pin 14
 instance = dht11.DHT11(pin=5)
 
-deviceId = 'wzaHMXe3G0NweRFW'
+deviceId = 'DTQj4ROiclK7aT4K'
 
 while True:
     result = instance.read()
     if result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
+        print("%s,%dC,%d%%" % (str(datetime.datetime.now()), result.temperature, result.humidity))
+        
+        try:
+            r = requests.post("https://wettycat.at.tw/api/device/insertData", json={
+                "deviceId": deviceId,
+                "data":{
+                    "humidity": result.humidity,
+                    "temperature": result.temperature
+                }
+            })
+        except:
+            print('Upload Failed')
 
-        r = requests.post("http://localhost/api/device/insertData", json={
-            "deviceId": deviceId,
-            "data":{
-                "humidity": result.humidity,
-                "temparature": result.temperature
-            }
-        })
-
-    time.sleep(1)
+    time.sleep(2)
