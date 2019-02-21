7b918fd935fa301544d8cbb1e241ce094ae9ead2
pp253 <pp.pp253@gmail.com>
2019-02-21 17:38:58 +0800

    update api domain

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ instance = dht11.DHT11(pin=5)
 deviceId = 'DTQj4ROiclK7aT4K'
 
 while True:
-    result = instance.read()
-    if result.is_valid():
-        print("%s,%dC,%d%%" % (str(datetime.datetime.now()), result.temperature, result.humidity))
-        
-        try:
-            r = requests.post("https://wettycat.at.tw/api/device/insertData", json={
-                "deviceId": deviceId,
-                "data":{
-                    "humidity": result.humidity,
-                    "temperature": result.temperature
-                }
-            })
-        except:
-            print('Upload Failed')
+  result = instance.read()
+  if result.is_valid():
+    print("%s,%dC,%d%%" % (str(datetime.datetime.now()),
+                           result.temperature, result.humidity))
 
-    time.sleep(2)
+    try:
+      r = requests.post("https://wettycat.duckdns.org/api/device/insertData", json={
+          "deviceId": deviceId,
+          "data": {
+              "humidity": result.humidity,
+              "temperature": result.temperature
+          }
+      })
+    except:
+      print('Upload Failed')
+
+  time.sleep(2)
