84661e5b4054b70c6d6590dccf32b239a808f3d0
pp253 <pp.pp253@gmail.com>
2019-03-26 18:20:03 +0800

    build led

--- main.py
+++ main.py
@@ -2,33 +2,58 @@ import ASUS.GPIO as GPIO
 import dht11
 import time
 import datetime
-import requests
+
+# config
+DHT11_PIN = 5
+LED_G_PIN = 8
+LED_Y_PIN = 10
+LED_R_PIN = 12
+DETECT_INTERVAL = 5
 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.cleanup()
 
-# read data using pin 14
-instance = dht11.DHT11(pin=5)
+GPIO.setup(LED_G_PIN, GPIO.OUT)
+GPIO.setup(LED_Y_PIN, GPIO.OUT)
+GPIO.setup(LED_R_PIN, GPIO.OUT)
 
-deviceId = 'DTQj4ROiclK7aT4K'
+
+def resetled():
+  GPIO.output(LED_G_PIN, GPIO.LOW)
+  GPIO.output(LED_Y_PIN, GPIO.LOW)
+  GPIO.output(LED_R_PIN, GPIO.LOW)
+
+
+resetled()
+
+# read dht11 data
+instance = dht11.DHT11(pin=DHT11_PIN)
 
 while True:
   result = instance.read()
   if result.is_valid():
-    print("%s,%dC,%d%%" % (str(datetime.datetime.now()),
-                           result.temperature, result.humidity))
-
     try:
-      r = requests.post("https://wettycat.duckdns.org/api/device/insertData", json={
-          "deviceId": deviceId,
-          "data": {
-              "humidity": result.humidity,
-              "temperature": result.temperature
-          }
-      })
-    except:
-      print('Upload Failed')
+      t = result.temperature
+      h = result.humidity
 
-  time.sleep(2)
+      print("%s,%dC,%d%%" % (str(datetime.datetime.now()),
+                             t, h))
+
+      if 40 <= h <= 60:
+        # Good
+        resetled()
+        GPIO.output(LED_G_PIN, GPIO.HIGH)
+      elif 60 < h <= 75 or 40 > h >= 35:
+        # warning
+        resetled()
+        GPIO.output(LED_Y_PIN, GPIO.HIGH)
+      else:
+        # bad
+        resetled()
+        GPIO.output(LED_R_PIN, GPIO.HIGH)
+    except:
+      print('Failed')
+
+  time.sleep(DETECT_INTERVAL)
